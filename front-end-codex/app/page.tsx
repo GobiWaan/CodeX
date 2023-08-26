b4e794f530a3fb22c4b58abb2ea72341137b0d24
Alexandre Gobeil <alexgobheb@gmail.com>
2023-08-26 19:53:10 -0400

    New Template

--- front-end-codex/app/page.tsx
+++ front-end-codex/app/page.tsx
@@ -3,11 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main >
-<<<<<<< HEAD
-      <h1>Allo</h1>
-=======
-      <h1>On va bientot etre riche</h1>
->>>>>>> 45ace5854f4bd3764003240dc9507220df1ef04f
+      <h1>test</h1>
     </main>
   )
 }
